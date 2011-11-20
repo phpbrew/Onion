946f0635e831724d5603a8ec2d5a656806c15023
c9s <cornelius.howl@gmail.com>
2011-11-20 12:22:33 +0800

    command dispatcher constructure

--- src/CLIFramework/CommandDispatcher.php
+++ src/CLIFramework/CommandDispatcher.php
@@ -8,14 +8,19 @@
  * file that was distributed with this source code.
  *
  */
+use CLIFramework\CommandContext;
 
 class CommandDispatcher 
 {
+    public $context;
 
     function __construct( $arg )
     {
         if( is_array( $arg ) ) {
-
+            $this->context = new CommandContext($arg);
+        }
+        elseif( is_a( $arg, 'CLIFramework\CommandContext' ) ) {
+            $this->context = $arg;
         }
     }
 
