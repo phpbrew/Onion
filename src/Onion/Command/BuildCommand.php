b1decf3e69c86599773e9e1313bd1a9a9e385d1b
c9s <cornelius.howl@gmail.com>
2011-11-20 14:29:54 +0800

    basic build command.

--- src/Onion/Command/BuildCommand.php
+++ src/Onion/Command/BuildCommand.php
@@ -8,3 +8,34 @@
  * file that was distributed with this source code.
  *
  */
+namespace Onion\Command;
+use CLIFramework\Command;
+use CLIFramework\CommandInterface;
+
+class BuildCommand extends Command 
+    implements CommandInterface
+{
+    function execute($context) 
+    {
+        $home = getenv('HOME');
+        $config_path = $home . DIRECTORY_SEPARATOR . '.onion.ini';  # ~/.onion.ini
+        if( ! file_exists( $config_path ) ) {
+            // create an skeleton for user and exit.
+            $default = <<<CONFIG
+[author]
+name = Your Name
+email = your@email
+CONFIG;
+            file_put_contents( $config_path , $default );
+            echo "please edit your config file first.\n";
+            echo "   \$ vim $config_path\n";
+            return true;
+        }
+
+
+        $author_config = parse_ini_file( $config_path );
+        var_dump( $author_config ); 
+        return true;
+    }
+}
+
