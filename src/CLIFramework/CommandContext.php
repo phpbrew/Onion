51e6031c9360b332ded5875a38d79acece6365d5
c9s <cornelius.howl@gmail.com>
2011-11-20 12:17:07 +0800

    CLIFramework

---
+++ src/CLIFramework/CommandContext.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * This file is part of the CLIFramework package.
+ *
+ * (c) Yo-An Lin <cornelius.howl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+namespace CLIFramework;
+
+class CommandContext 
+{
+    public $argv;
+    public $script;
+    public $context_list;
+
+    function __construct($argv)
+    {
+        $this->argv         = array_merge(array(), $argv);
+        $this->script       = $argv[0];
+        $this->context_list = array_slice($argv,1); # copy and shift one
+    }
+
+    function shiftArgument()
+    {
+        return array_shift($this->context_list);
+    }
+
+    function getNextArgument()
+    {
+        return $this->context_list[0];
+    }
+
+}
+
---
+++ src/CLIFramework/CommandDispatcher.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the CLIFramework package.
+ *
+ * (c) Yo-An Lin <cornelius.howl@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ */
+
+class CommandDispatcher 
+{
+
+    function __construct( $arg )
+    {
+        if( is_array( $arg ) ) {
+
+        }
+    }
+
+}
+
