468102e62455c8fe38d1c9394527a3d8ecfd23b8
c9s <cornelius.howl@gmail.com>
2011-12-12 01:37:43 +0800

    Add TestApp and update classlaoder

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,7 @@ require 'UniversalClassLoader/SplClassLoader.php';
 $classLoader = new \UniversalClassLoader\SplClassLoader(array( 
     'Onion' => 'src',
     'CLIFramework' => 'src',
+    'TestApp' => 'tests/TestApp',
 ));
 $classLoader->useIncludePath(true);
 $classLoader->register();
