fbf331de8f6cf5edf594a2dbc559a974b756a788
c9s <cornelius.howl@gmail.com>
2011-12-12 02:21:31 +0800

    Clean up command dispatcher.

--- src/CLIFramework/CommandDispatcher.php
+++ src/CLIFramework/CommandDispatcher.php
@@ -36,27 +36,6 @@ class CommandDispatcher
         return $class;
     }
 
-    public function runDispatch()
-    {
-        $command = $this->context->shiftArgument();
-        return $this->dispatch($command);
-    }
-
-    /* dispatch comamnd , the entry point */
-    public function dispatch( $command = null )
-    {
-        if( $command ) 
-        {
-            $class = $this->getCommandClass($command);
-            $cmd = new $class($this);
-            return $cmd->topExecute($this->context);
-        } 
-        else 
-        {
-            return $this->dispatch('help');  # dispatch to help command class.
-        }
-    }
-
     public function getSubcommandClass($subcommand)
     {
         return $this->loader->loadSubcommand( $subcommand );
@@ -65,21 +44,6 @@ class CommandDispatcher
     public function hasSubcommand($subcommand)
     {
         return $this->getSubcommandClass($subcommand) ? true : false;
-    }
-
-    public function shiftDispatch($parent)
-    {
-        $subcommand = $this->context->getNextArgument();
-        if( $class = $this->getSubcommandClass($subcommand) )  {
-            $this->context->shiftArgument();
-
-            // re-dispatch context to subcommand class.
-            $cmd = new $class($this);
-            return $cmd->topExecute($this->context);
-        }
-        else {
-            throw new Exception( "Subcommand '$subcommand' not found." );
-        }
     }
 
 }
