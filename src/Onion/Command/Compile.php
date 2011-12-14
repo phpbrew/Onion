e5e7f2724a7683af606a5d5eb3081741c4bfb08f
c9s <cornelius.howl@gmail.com>
2011-12-14 19:54:45 +0800

    Add compile command.

---
+++ src/Onion/Command/Compile.php
@@ -0,0 +1,31 @@
+<?php
+namespace Onion\Command;
+
+class CompileCommand 
+{
+    function options($opts)
+    {
+        $opts->add('autoload:','autoload source file');
+        $opts->add('executable:','executable source file');
+        $opts->add('src+','external source dir');
+        $opts->add('output:','output');
+    }
+
+    function brief()
+    {
+        return 'compile current source into a phar file.';
+    }
+
+    function execute($arguments)
+    {
+        $options = $this->getOptions();
+        $executable = $options->executable;
+        $src_dirs   = $options->src;
+        $output     = $options->output;
+        $src_dirs = array_unshift( $src_dirs , 'src' );
+
+        
+
+
+    }
+}
