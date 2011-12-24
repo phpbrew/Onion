a6a6bd0df76d933346e77ab6ef21ef65d3730b44
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-25 08:45:04 +0900

    Fixed to use vendor.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 require 'tests/helpers.php';
-require 'Universal/ClassLoader/SplClassLoader.php';
+require __DIR__ . '/../vendor/pear/Universal/ClassLoader/SplClassLoader.php';
 $classLoader = new \Universal\ClassLoader\SplClassLoader(array( 
     'Onion' => 'src',
+    'CLIFramework' => 'vendor/pear',
+    'GetOptionKit' => 'vendor/pear',
     'TestApp' => 'tests',
     'Pyrus' => '/Users/c9s/git/others/php/pyrus/Pyrus/src',
 ));
