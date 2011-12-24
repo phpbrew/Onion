5c5c28efdbcfc40b9450748c690dcfcff0dce1cd
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2011-12-25 08:49:12 +0900

    Inserted linebreaks into command-line options.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# ./onion.phar compile --lib src --lib ../CLIFramework/src --lib ../GetOptionKit/src --classloader --bootstrap scripts/onion.embed --executable --compress=bz2 --output onion.phar
-./scripts/onion -d compile --lib src --lib ../CLIFramework/src --lib ../GetOptionKit/src --classloader --bootstrap scripts/onion.embed --executable --output onion.phar
+./scripts/onion -d compile \
+    --lib src \
+    --lib ../CLIFramework/src \
+    --lib ../GetOptionKit/src \
+    --classloader \
+    --bootstrap scripts/onion.embed \
+    --executable \
+    --output onion.phar
