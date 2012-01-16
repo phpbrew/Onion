ba5f857e4c29cb0d92f764c91a0e47f257380780
c9s <cornelius.howl@gmail.com>
2012-01-16 12:02:28 +0800

    Fix define

--- tests/helpers.php
+++ tests/helpers.php
@@ -9,6 +9,10 @@
  *
  */
 
+if( ! defined('DEBUG_BACKTRACE_PROVIDE_OBJECT') )
+    define('DEBUG_BACKTRACE_PROVIDE_OBJECT',1);
+
+
 function ok( $v , $msg = null )
 {
     $stacks = debug_backtrace( DEBUG_BACKTRACE_PROVIDE_OBJECT ); # XXX: limit is only availabel in PHP5.4
