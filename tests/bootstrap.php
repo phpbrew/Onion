bec2dc72f64ed2ca1ec67d1371886a65b99ef9e4
c9s <cornelius.howl@gmail.com>
2012-03-19 19:02:52 +0800

    Fix vendor path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,11 +3,11 @@ require 'tests/helpers.php';
 require 'vendor/pear/Universal/ClassLoader/SplClassLoader.php';
 $classLoader = new \Universal\ClassLoader\SplClassLoader(array( 
     'Onion' => 'src',
+    'CacheKit'     => 'vendor/pear',
     'CLIFramework' => 'vendor/pear',
-    'CacheKit'     => 'vendro/pear',
     'GetOptionKit' => 'vendor/pear',
     'TestApp' => 'tests',
     // 'Pyrus' => '/Users/c9s/git/others/php/pyrus/Pyrus/src',
 ));
-$classLoader->useIncludePath(true);
+$classLoader->useIncludePath(false);
 $classLoader->register();
