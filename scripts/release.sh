40f148ec84de2c89295c86485cb0a66d1436f397
c9s <cornelius.howl@gmail.com>
2012-03-19 19:24:44 +0800

    Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+rm -rf .onion/
+rm -rf vendor/
 
 # bundle with new dependencies
-./onion -d bundle || exit
+php onion bundle || exit
 
 # compile to phar file
 scripts/compile.sh || exit
 
 # build new package.xml
-./onion -d build --pear || exit
+php onion -d build || exit
 
 # use pear to install 
 sudo pear install -a -f package.xml || exit
