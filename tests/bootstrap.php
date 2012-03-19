0097dd80ff87d9c3247f4f1fb4451d8de95cd7ed
c9s <cornelius.howl@gmail.com>
2012-03-19 19:00:15 +0800

    No pyrus

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 require 'tests/helpers.php';
-require __DIR__ . '/../vendor/pear/Universal/ClassLoader/SplClassLoader.php';
+require 'vendor/pear/Universal/ClassLoader/SplClassLoader.php';
 $classLoader = new \Universal\ClassLoader\SplClassLoader(array( 
     'Onion' => 'src',
     'CLIFramework' => 'vendor/pear',
     'CacheKit'     => 'vendro/pear',
     'GetOptionKit' => 'vendor/pear',
     'TestApp' => 'tests',
-    'Pyrus' => '/Users/c9s/git/others/php/pyrus/Pyrus/src',
+    // 'Pyrus' => '/Users/c9s/git/others/php/pyrus/Pyrus/src',
 ));
 $classLoader->useIncludePath(true);
 $classLoader->register();
