1bc7ccd591f9dffbd74470aecc4d10d6987aaabf
c9s <cornelius.howl@gmail.com>
2012-06-09 13:01:04 +0800

    check download size

--- src/CurlKit/Progress/ProgressBar.php
+++ src/CurlKit/Progress/ProgressBar.php
@@ -8,6 +8,14 @@ class ProgressBar
 
     function callback($downloadSize, $downloaded, $uploadSize, $uploaded)
     {
+        if( $downloadSize < $this->showSize ) {
+            return;
+        }
+        if( $this->done ) {
+            return;
+        }
+
+
         // print progress bar
         $percent = ($downloaded > 0 ? (float) ($downloaded / $downloadSize) : 0.0 );
         $terminalWidth = 70;
@@ -18,7 +26,10 @@ class ProgressBar
             str_repeat( '#' , $sharps ) . 
             str_repeat( ' ' , $terminalWidth - $sharps ) . 
             sprintf( ' %4d B %5d%%' , $downloaded , $percent * 100 );
-    }
 
+        if( $downloadSize != 0 && $downloadSize === $downloaded ) {
+            echo "\n";
+        }
+    }
 }
 
